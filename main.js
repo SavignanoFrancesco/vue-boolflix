cfef551edf557bbae550d712fe3f87e24712f346
Francesco <francesco.savignano96@gmail.com>
2020-11-24 14:55:22 +0100

    aggiunto evento con tasto invio e alert se non si scrive niente nell'input

--- main.js
+++ main.js
@@ -7,20 +7,24 @@ var app = new Vue({
     methods: {
         movieRequest(){
             const self = this;
-            axios
-                .get('https://api.themoviedb.org/3/search/movie', {
-                    params:
-                        {
-                            api_key: '26a07cb4c3a1c1a713d00530e848c684',
-                            query: self.movie_searched,
-                            language: 'it'
-                        }
-                    })
-                .then(function(risposta) {
+            if (self.movie_searched != '') {
+                axios
+                    .get('https://api.themoviedb.org/3/search/movie', {
+                        params:
+                            {
+                                api_key: '26a07cb4c3a1c1a713d00530e848c684',
+                                query: self.movie_searched,
+                                language: 'it'
+                            }
+                        })
+                    .then(function(risposta) {
 
-                    self.movies = risposta.data.results;
-                    console.log(self.movies);
-                });
+                        self.movies = risposta.data.results;
+                        console.log(self.movies);
+                    });
+            }else{
+                alert('Devi inserire qualcosa nell\'input!!');
+            }
         }
     },
     mounted() {
