581f0a40de3b007af0ad602149c7dbb5aa44796f
Francesco <francesco.savignano96@gmail.com>
2020-11-24 17:00:38 +0100

    aggiunte serie tv

--- main.js
+++ main.js
@@ -2,21 +2,22 @@ var app = new Vue({
     el: '#root',
     data: {
         movies: [],
-        movie_searched: '',
+        title_searched: '',
+        tvshows: [],
         max_vote: 5,
         known_languages: ['en','it','fr','es','ja','de']
     },
     methods: {
         movieRequest(){
             const self = this;
-            if (self.movie_searched != '') {
+            if (self.title_searched != '') {
                 //richiesta al server
                 axios
                     .get('https://api.themoviedb.org/3/search/movie', {
                         params:
                             {
                                 api_key: '26a07cb4c3a1c1a713d00530e848c684',
-                                query: self.movie_searched,
+                                query: self.title_searched,
                                 language: 'it'
                             }
                         })
@@ -36,9 +37,38 @@ var app = new Vue({
                 alert('Devi inserire qualcosa nell\'input!!');
             }
         },
-        getLanguageFlag(movie){
+        tvshowsRequest(){
+            const self = this;
+            if (self.title_searched != '') {
+                //richiesta al server
+                axios
+                    .get('https://api.themoviedb.org/3/search/tv', {
+                        params:
+                            {
+                                api_key: '26a07cb4c3a1c1a713d00530e848c684',
+                                query: self.title_searched,
+                                language: 'it'
+                            }
+                        })
+                    .then(function(risposta) {
 
-            return 'bandiere/' + movie.original_language + '.jpg';
+                        //metto la risposta dell'API in un array
+                        self.tvshows = risposta.data.results;
+
+                        //trasformo i voti da base 10 a base 5
+                        self.tvshows.forEach((show, i) => {
+                            show.vote_average = Math.round(show.vote_average / 2);
+                        });
+
+                        console.log(self.tvshows);
+                    });
+            }else{
+                alert('Devi inserire qualcosa nell\'input!!');
+            }
+        },
+        getLanguageFlag(title){
+
+            return 'bandiere/' + title.original_language + '.jpg';
 
         }
     },
